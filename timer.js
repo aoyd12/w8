93ac10ad25628e36c47adff4a367cc947bdb8bf3
aoyd12 <95006309+aoyd12@users.noreply.github.com>
2022-01-13 11:37:53 +0900

    Update timer.js

--- timer.js
+++ timer.js
@@ -24,6 +24,6 @@ let button1 = document.getElementById('startcount');
 button1.addEventListener('click', startShowing);
 
 
-let button2 = document.getElementById('startcount');
+let button2 = document.getElementById('stopcount');
 button2.addEventListener('click', stopShowing);
 
